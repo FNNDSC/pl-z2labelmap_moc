ee09961b7d9dff3ea0a49e2cc4e0950e58c5a527
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2019-10-01 16:20:17 -0400

    Add moc specific deltas.

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 
 import sys
 import os
-
-
 # Make sure we are running python3.5+
-if 10 * sys.version_info[0] + sys.version_info[1] < 35:
+if 10 * sys.version_info[0]  + sys.version_info[1] < 35:
     sys.exit("Sorry, only Python 3.5+ is supported.")
-
 
 from setuptools import setup
 
@@ -18,20 +15,20 @@ def readme():
         return f.read()
 
 setup(
-      name             =   'z2labelmap_moc',
+      name             =   'z2lablelmap_moc',
       # for best practices make this version the same as the VERSION class variable
       # defined in your ChrisApp-derived Python class
-      version          =   '0.1',
-      description      =   'An app to ...',
+      version          =   '2.0.1',
+      description      =   'Convert lh/rh z-score vector to FreeSurfer labelmap', 
       long_description =   readme(),
-      author           =   'FNNDSC',
-      author_email     =   'dev@babyMRI.org',
-      url              =   'http://wiki',
-      packages         =   ['z2labelmap_moc'],
-      install_requires =   ['chrisapp', 'pudb'],
+      author           =   'Rudolph Pienaar',
+      author_email     =   'rudolph.pienaar@gmail.com',
+      url              =   'https://github.com/FNNDSC/pl-z2lablelmap_moc',
+      packages         =   ['z2lablelmap_moc'],
+      install_requires =   ['chrisapp', 'pandas', 'numpy', 'pudb'],
       test_suite       =   'nose.collector',
       tests_require    =   ['nose'],
-      scripts          =   ['z2labelmap_moc/z2labelmap_moc.py'],
+      scripts          =   ['z2lablelmap_moc/z2lablelmap_moc.py'],
       license          =   'MIT',
       zip_safe         =   False
      )
